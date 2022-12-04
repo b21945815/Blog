ca77625fd42247713f828da1846935a8fc425229
b21945815 <72704627+b21945815@users.noreply.github.com>
2022-12-04 13:24:25 +0300

    Delete HelloWorld.py

--- HelloWorld.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-header = st.container()
-usingModel = st.container()
-with header:
-    st.title("Looking for predictions of cars' price")
-    st.text("This is just a extra line to show you the difference between these functions")
-with usingModel:
-    st.title("Input to estimate the cars' price")
